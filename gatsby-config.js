935bffc9a10af3e73d52f532ba9395bc1339d4ec
Andreas Weis <thabeat666@gmail.com>
2020-07-07 11:35:00 +1000

    Fixed robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,18 @@
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://andreasweis.com',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 module.exports = {
   siteMetadata: {
     title: `Andreas Weis — Product designer and frontend developer`,
     description: `Personal website of Andreas Weis`,
     author: `@andi_weis`,
+    siteUrl,
   },
   plugins: [
     `gatsby-plugin-sass`,
@@ -22,8 +32,22 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-robots-txt',
       options: {
-        host: 'https://andreasweis.com',
-        policy: [{ userAgent: '*', allow: '/' }]
+        resolveEnv: () => NETLIFY_ENV,
+        env: {
+          production: {
+            policy: [{ userAgent: '*' }]
+          },
+          'branch-deploy': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          },
+          'deploy-preview': {
+            policy: [{ userAgent: '*', disallow: ['/'] }],
+            sitemap: null,
+            host: null
+          }
+        }
       }
     },
   ],
