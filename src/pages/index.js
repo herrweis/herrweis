0d8ecb67222e47933b33f5bca55a549734b4824e
herrweis <thabeat666@gmail.com>
2024-09-16 11:47:29 +1000

    hide portfolio on front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default function Page() {
         I have worked with <a className="custom" href="//bendigobank.com.au" aria-label="Go to Bendigo Bank's website">Bendigo Bank</a>, <a href="//ferocia.com.au" aria-label="Go to Ferocia's website">Ferocia</a>, <a href="//up.com.au" aria-label="Go to Up's website">Up</a>, <a href="//unimelb.edu.au" aria-label="Go to University of Melbourne's website">University of Melbourne</a>, and other awesome places. <a href="https://www.linkedin.com/in/andi-weis-b114a115/" aria-label="Connect with me on LinkedIn">Find me on LinkedIn</a> or <a href="mailto:post@andreasweis.com?subject=👋" aria-label="Write me an email">write me an email</a>.
       </p>
 
-      <PortfolioLinks />
+      {/* <PortfolioLinks /> */}
 
       <AnimatedCursor
         innerSize={44}
