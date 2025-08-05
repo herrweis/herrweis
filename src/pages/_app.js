11e0751175d5e85424883b24374cffcc94b1397d
herrweis <thabeat666@gmail.com>
2025-08-06 08:56:16 +1000

    Fixed google and css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,47 +2,13 @@ import '../styles/font.css'
 import '../styles/global.scss'
 import '../styles/work.scss'
 import { AppProps } from 'next/app'
-import Head from 'next/head'
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      window.gtag('config', 'G-XXXXXXXXXX', {
-        page_path: url,
-      })
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
   return (
     <>
-      <Head>
-        {/* Google Analytics */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-2TS82343BJ"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-2TS82343BJ', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-      </Head>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-2TS82343BJ" />
     </>
   )
 }
