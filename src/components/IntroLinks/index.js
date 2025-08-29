b4debb78635c9d3968a2315e9e4c0a72fe5474ab
herrweis <thabeat666@gmail.com>
2025-08-29 15:47:31 +1000

    Changed availabitlity to october

--- src/components/IntroLinks/index.js
+++ src/components/IntroLinks/index.js
@@ -7,7 +7,7 @@ const IntroLinks = () => {
                 Previously at <a href="//ferocia.com.au" aria-label="Visit Ferocia's website">Ferocia</a> &amp; <a href="//up.com.au" aria-label="Visit Up's website">Up</a>, <a href="//bendigobank.com.au" aria-label="Visit Bendigo Bank's website">Bendigo Bank</a>, <a href="//kmart.com.au" aria-label="Visit Kmart's website">Kmart</a>, <a href="//lyrebirdhealth.com" aria-label="Visit Lyrebird Health">Lyrebird Health</a> and the <a href="//unimelb.edu.au" aria-label="Visit University of Melbourne's website">University of Melbourne</a> amongst&nbsp;other&nbsp;awesome&nbsp;places.
             </p>
             <p className="links">
-                I have one spot open in September. <a href="https://www.linkedin.com/in/andi-weis/" aria-label="Connect with me on LinkedIn">Connect on LinkedIn</a> or <a href="mailto:post@andreasweis.com?subject=👋" aria-label="Write me an email">drop me an email</a>.
+                I have one spot open in October. <a href="https://www.linkedin.com/in/andi-weis/" aria-label="Connect with me on LinkedIn">Connect on LinkedIn</a> or <a href="mailto:post@andreasweis.com?subject=👋" aria-label="Write me an email">drop me an email</a>.
             </p>
         </div>
     )
