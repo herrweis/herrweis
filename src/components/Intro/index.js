d277f917d5ba23c5f9f193d417da96dbc0fe17a4
herrweis <thabeat666@gmail.com>
2025-12-03 21:01:27 +1100

    Deleted CV links

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -4,7 +4,7 @@ import React from "react"
 const Intro = () => {
     return (
         <p className="intro">
-           Designer, maker, and collaborator with 23+ years creating digital products, strategies, and teams that matter. Based in Melbourne/Narrm, now working independently on strategic design consulting and fractional leadership where I can help build great things.
+           Designer, maker, and collaborator with 23+ years creating digital products, strategies, and teams that matter. Based in Melbourne/Narrm, working independently on strategic design consulting and fractional leadership where I can help build great things.
         </p>
     )
 
--- src/components/IntroLinks/index.js
+++ src/components/IntroLinks/index.js
@@ -3,9 +3,9 @@ const IntroLinks = () => {
 
     return (
         <div>
-            <p className="links">
+            {/* <p className="links">
                 Previously at <a href="//ferocia.com.au" aria-label="Visit Ferocia's website">Ferocia</a> &amp; <a href="//up.com.au" aria-label="Visit Up's website">Up</a>, <a href="//bendigobank.com.au" aria-label="Visit Bendigo Bank's website">Bendigo Bank</a>, <a href="//kmart.com.au" aria-label="Visit Kmart's website">Kmart</a>, <a href="//lyrebirdhealth.com" aria-label="Visit Lyrebird Health">Lyrebird Health</a> and the <a href="//unimelb.edu.au" aria-label="Visit University of Melbourne's website">University of Melbourne</a> amongst&nbsp;other&nbsp;awesome&nbsp;places.
-            </p>
+            </p> */}
             <p className="links">
                 {availability}. <a href="//www.linkedin.com/in/andi-weis/" aria-label="Connect with me on LinkedIn">Connect on LinkedIn</a> or <a href="mailto:post@andreasweis.com?subject=👋" aria-label="Write me an email">drop me an email</a>.
             </p>
